bdeff01f780868452339d98337102b978da21bf8
jacobhumston <humstonjacob@gmail.com>
2021-06-09 11:28:05 -0400

    fix

--- config.lua
+++ config.lua
@@ -2,8 +2,8 @@ return {
     prefixes = {
         "l!",
         "<@614932167373357097>",
-        "<@!614932167373357097>" 
-    }
+        "<@!614932167373357097>",
+    },
     -- Feel free to add as many prefixes as you want.
     ownerid = "539194357706653696" 
     -- Set this to your Discord ID.
