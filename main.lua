2a7a5f3db966c75b87cf1f0031f09d621bb9d750
jacobhumston <humstonjacob@gmail.com>
2021-06-09 12:56:10 -0400

    test

--- main.lua
+++ main.lua
@@ -6,7 +6,7 @@ local token = require("token")
 local config = require("config")
 
 client:on('ready', function()
-    client:setStatus("test")
+    client:setStatus("hmmmmm")
     print("Ready!")
 end)
 
