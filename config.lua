e7e0ef1ce3921050cf7349f5337ec6f704b346b5
jacobhumston <humstonjacob@gmail.com>
2021-06-09 11:32:41 -0400

    Added spaces to mention prefixes.

--- config.lua
+++ config.lua
@@ -1,8 +1,8 @@
 return {
     prefixes = {
         "l!",
-        "<@614932167373357097>",
-        "<@!614932167373357097>",
+        "<@614932167373357097> ", -- Change the numbers to your bot's ID if you decide to keep these prefixes.  
+        "<@!614932167373357097> ",
     },
     -- Feel free to add as many prefixes as you want.
     ownerid = "539194357706653696" 
