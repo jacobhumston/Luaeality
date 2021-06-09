5ee378b02d5962c954ccac8a1824cb503dc550e8
jacobhumston <humstonjacob@gmail.com>
2021-06-09 06:03:19 -0400

    -

---
+++ main.lua
@@ -0,0 +1,16 @@
+local discordia = require('discordia')
+local client = discordia.Client()
+local token = require("token")
+
+client:on('ready', function()
+	print('Logged in as '.. client.user.username)
+end)
+
+client:on('messageCreate', function(message)
+    local prefix = "!"
+    if string.lower(message.content) == prefix.."help" then
+        message:reply("I'm here! <3")
+    end
+end)
+
+client:run("Bot "..token.Token)
