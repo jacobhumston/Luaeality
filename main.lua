167ca1d7b2d2c6fc7baa7f46b1e6c4fc32cd7d45
jacobhumston <humstonjacob@gmail.com>
2021-06-09 13:19:24 -0400

    fix

--- main.lua
+++ main.lua
@@ -29,6 +29,7 @@ client:on('messageCreate', function(message)
         message:addReaction(client:getEmoji(852216908362940416))
         message.channel:send{
         reference = message.id,
+        mention = message.author,
         embed = {
             title = "Help",
             description = "<:Bot:852216908362940416> Hello! My name is Luaeality and I'm an [open source](https://github.com/jacobhumston/Luaeality) Discord bot written in Lua.",
