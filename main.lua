b633ffbb464be30fd6bd486f413a000c7fe127a2
jacobhumston <humstonjacob@gmail.com>
2021-06-10 10:17:43 -0400

    fix

--- main.lua
+++ main.lua
@@ -30,7 +30,7 @@ client:on('messageCreate', function(message)
             if output == "" then
                 output = "`"..v.."`"
             else
-                output = output.." ,`"..v.."`"
+                output = output..", `"..v.."`"
             end
         end
         return output
@@ -43,7 +43,7 @@ client:on('messageCreate', function(message)
         },
         embed = {
             title = "Help",
-            description = "<:Bot:852216908362940416> Hello! My name is Luaeality and I'm an [open source](https://github.com/jacobhumston/Luaeality) Discord bot written in Lua.<:Support:852216908383125535> If you need support: https://discord.gg/2yyQygXP8u\n<:Star:852216908341051432> **Prefixes:** "..getprefixes(),
+            description = "<:Bot:852216908362940416> Hello! My name is Luaeality and I'm an [open source](https://github.com/jacobhumston/Luaeality) Discord bot written in Lua.<:Support:852216908383125535> If you need support: https://discord.gg/2yyQygXP8u\n<:Star:852216908341051432> Prefixes: "..getprefixes(),
             color = 0x6666ff,
         }}
     end
