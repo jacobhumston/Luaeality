f4b4b0eaea4f498502e4cd545d5526c86e7e1106
jacobhumston <humstonjacob@gmail.com>
2021-06-18 12:11:02 -0400

    -

--- main.lua
+++ main.lua
@@ -23,7 +23,18 @@ client:on('messageCreate', function(message)
         end
         return nil
     end
-    
+    if hasprefix("help") then
+        message.channel:send{
+            reference = {
+                message = message.id,
+            },
+            embed = {
+                title = "Help",
+                description = "<:Bot:852216908362940416> Hello! My name is Luaeality and I'm an [open source](https://github.com/jacobhumston/Luaeality) Discord bot written in Lua.<:Support:852216908383125535> If you need support: https://discord.gg/2yyQygXP8u\n<:Star:852216908341051432> Prefixes: "..getprefixes(),
+                color = 0x6666ff,
+            }
+        }
+    end
     
 end)
 
